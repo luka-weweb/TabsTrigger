cb63b52f67f60db3ef6cf168c4c77223155d38bc
luka-weweb <luka@weweb.io>
2024-08-26 15:53:02 +0200

    fix

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,20 +1,25 @@
 <script>
 export default {
-  inject: ["activeTabProvided", "setActiveTab"],
+  inject: ["activeTabProvided", "setActiveTab", "registerTabTrigger"],
   props: {
     content: { type: Object, required: true },
     wwFrontState: { type: Object, required: true },
     wwEditorState: { type: Object, required: true },
   },
-  methods: {
-    handleClick() {
-      this.setActiveTab(this.content.name);
+  computed: {
+    isSelected() {
+      return this.activeTabProvided === this.content.name;
     },
+  },
+  methods: {
     handleFocus() {
-      if (this.content.acivation === "automatic") {
+      if (this.content.activation === "automatic") {
         this.setActiveTab(this.content.name);
       }
     },
+  },
+  mounted() {
+    this.registerTabTrigger(this.content.name, this.$el);
   },
 };
 </script>
@@ -22,16 +27,11 @@ export default {
 <template>
   <button
     role="tab"
-    :aria-selected="isSelected ? 'true' : 'false'"
-    @click="handleClick"
+    :aria-selected="isSelected"
+    @click="setActiveTab(content.name)"
     @focus="handleFocus"
   >
-    <wwLayoutItemContext
-      :index="index"
-      :item="{}"
-      is-repeat
-      :data="{ active: activeTabProvided == this.content.name }"
-    >
+    <wwLayoutItemContext is-repeat :data="{ active: isSelected }">
       <wwLayout path="slot" />
     </wwLayoutItemContext>
   </button>
